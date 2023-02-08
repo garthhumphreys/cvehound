0d9e13f9d4c779682e0e8300a40fff8c3e70bcea
Garth Humphreys <g.humphreys1@mac.com>
2023-02-08 17:13:57 -0500

    remove compiled go code

--- cvehound.go
+++ cvehound.go
@@ -7,42 +7,55 @@ import (
 	"path/filepath"
 )
 
-const repo = "https://github.com/CVEProject/cvelist.git"
+const cveListRepo = "https://github.com/CVEProject/cvelist.git"
+const cveListLocalPath = "cvelist"
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: cvehound keyword")
-		os.Exit(1)
-	}
+var home string
+var cveListPath string
 
-	keyword := os.Args[1]
-
-	home := os.Getenv("HOME")
-	repoPath := filepath.Join(home, "cvelist")
-
-	if _, err := os.Stat(repoPath); os.IsNotExist(err) {
+func initCVERepo() {
+	if _, err := os.Stat(cveListPath); os.IsNotExist(err) {
 		fmt.Println("Cloning cvelist repository...")
-		err := exec.Command("git", "clone", repo, repoPath).Run()
+		err := exec.Command("git", "clone", cveListRepo, cveListPath).Run()
 		if err != nil {
 			fmt.Println("Error cloning repository:", err)
 			os.Exit(1)
 		}
 	} else {
 		fmt.Println("Updating cvelist repository...")
-		err := exec.Command("git", "-C", repoPath, "pull", "origin", "master").Run()
+		err := exec.Command("git", "-C", cveListPath, "pull", "origin", "master").Run()
 		if err != nil {
 			fmt.Println("Error updating repository:", err)
 			os.Exit(1)
 		}
 	}
+}
 
-	fmt.Println("Searching for keyword...")
-	regex_string := fmt.Sprintf("(\"TITLE\".*%s\")", keyword)
-	output, err := exec.Command("rg", regex_string, repoPath).CombinedOutput()
+func searchCVEList(searchTerm string) {
+	fmt.Println("Searching for Search Term...")
+	regex_string := fmt.Sprintf("(\"TITLE\".*%s\")", searchTerm)
+	output, err := exec.Command("rg", regex_string, cveListPath).CombinedOutput()
 	if err != nil {
-		fmt.Println("Error searching for keyword:", err)
+		fmt.Println("Error searching for cvelist searchTerm:", err)
 		os.Exit(1)
 	}
 
 	fmt.Println(string(output))
+}
+
+func init() {
+	home = os.Getenv("HOME")
+	cveListPath = filepath.Join(home, cveListLocalPath)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: cvehound searchTerm")
+		os.Exit(1)
+	}
+
+	searchTerm := os.Args[1]
+
+	initCVERepo()
+	searchCVEList(searchTerm)
 }
